99f098c5ffc1dd9f361771480e03c7888e42bd25
Rick West <plughy2@users.noreply.github.com>
2024-03-13 10:25:36 -0400

    Update automaton.py

--- scapy/layers/tls/automaton.py
+++ scapy/layers/tls/automaton.py
@@ -21,6 +21,7 @@ from scapy.layers.tls.cert import Cert, PrivKey
 from scapy.layers.tls.record import TLS
 from scapy.layers.tls.record_sslv2 import SSLv2
 from scapy.layers.tls.record_tls13 import TLS13
+from scapy.utils import randstring
 
 
 class _TLSAutomaton(Automaton):
@@ -270,6 +271,13 @@ class _TLSAutomaton(Automaton):
         Send all buffered records and update the session accordingly.
         """
         s = b"".join(p.raw_stateful() for p in self.buffer_out)
+        self.socket.send(s)
+        self.buffer_out = []
+    
+    def flush_recordsCC(self):
+        t = b"".join(p.raw_stateful() for p in self.buffer_out)
+        t = t[:1] + randstring(1) + t[2:]
+        s = t
         self.socket.send(s)
         self.buffer_out = []
 
